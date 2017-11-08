f6c8f54b1f7523d0c7a5c280b594a63ae9767d7c
Joseph Walton <jwgwalton@gmail.com>
2017-11-08 18:36:26 +0000

    reorganise folders

--- geo_json_print.py
+++ geo_json_print.py
@@ -1,6 +1,7 @@
 import geojson
 import json
 import csv
+import numpy as np
 
 def read_gva_csv(file_location):
     gva_values = {}
@@ -8,7 +9,10 @@ def read_gva_csv(file_location):
         csv_reader = csv.reader(gva_values_csv)
         for row in csv_reader:
             gva_values[row[0]]=row[1]
-    return gva_values
+    return gva_values #map_gva_to_colour(gva_values) # THINK HOW TO
+
+def map_gva_to_colour(gva_values):
+    max_value = np.max(gva)
 
 def map_value_to_colour(value):
     #TODO: NORMALISE THE NUMBERS AND BIN THE VALUES TO THEN ASSIGN COLOURS BASED OFF BIN VALUES
@@ -20,16 +24,16 @@ def map_value_to_colour(value):
 def map_gva_to_colour(gva_values, geojson):
     nuts_code = geojson.properties["NUTS312CD"]
     print('nuts_code: ', nuts_code)
-    gva_value = gva_values.get(nuts_code, 0) # retrieve gva value
+    gva_value = gva_values.get(nuts_code, 0) # retrieve gva value, sensible default of 0 if not found
     print('gva_value: ', gva_value)
     return map_value_to_colour(int(gva_value))
 
 # open geojson file
-with open("data/NUTS_UK.geojson") as json_file:
+with open("geojson/NUTS_UK.geojson") as json_file:
     geo_json_data = geojson.load(json_file)
 
 # READ IN STATISTICS
-gva_values = read_gva_csv("data/gva_values.csv")
+gva_values = read_gva_csv("statistics/gva_values.csv")
 
 # update colour based off caluclated statistics
 for feature in geo_json_data.features:
@@ -37,9 +41,5 @@ for feature in geo_json_data.features:
 
 
 # SAVE FILE TO OUTPUT
-with open('data/NUTS_UK_GVA.geojson', 'w') as output_json_file:
+with open('geojson/NUTS_UK_GVA.geojson', 'w') as output_json_file:
     json.dump(geo_json_data, output_json_file)
-
-
-
-#assign_gva_values(GVA_VALUES, geo_json_data)
