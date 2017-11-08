35ea3c438289ef49d29c9d895e8b9237142b9feb
Joseph Walton <jwgwalton@gmail.com>
2017-11-08 18:13:11 +0000

    add gva values from static csv

--- geo_json_print.py
+++ geo_json_print.py
@@ -1,21 +1,34 @@
 import geojson
 import json
+import csv
 
-GVA_VALUES = [{ "UKC11": 1000}, {"UKC12": 1001}]
+def read_gva_csv(file_location):
+    gva_values = {}
+    with open(file_location) as gva_values_csv:
+        csv_reader = csv.reader(gva_values_csv)
+        for row in csv_reader:
+            gva_values[row[0]]=row[1]
+    return gva_values
 
-def assign_gva_values(gva_values, geojson):
-    for value in gva_values:
-        print('value', value)
+def map_gva_to_colour(gva_values, geojson):
+    nuts_code = geojson.properties["NUTS312CD"]
+    print('nuts_code: ', nuts_code)
+    print('gva_value: ', gva_values.get(nuts_code, "blue"))
+    return gva_values.get(nuts_code, "blue")
 
 # open geojson file
 with open("data/NUTS_UK.geojson") as json_file:
     geo_json_data = geojson.load(json_file)
 
+# READ IN STATISTICS
+# TODO: ACTUALLY PULL FROM NON STATIC FILE
+gva_values = read_gva_csv("data/gva_values.csv")
+
 # update colour based off caluclated statistics
 # TODO: ACTUALLY PULL STATISTICS AND THEN MAP TO A COLOUR
 for feature in geo_json_data.features:
-    #print(feature)
-    feature.properties["fill"] = "red"
+    feature.properties["fill"] = map_gva_to_colour(gva_values, feature)
+
 
 # SAVE FILE TO OUTPUT
 with open('data/NUTS_UK_GVA.geojson', 'w') as output_json_file:
