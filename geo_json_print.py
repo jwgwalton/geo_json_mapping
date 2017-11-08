1097e0de27c9067a7a024e91b2611a899fecbbfb
Joseph Walton <jwgwalton@gmail.com>
2017-11-08 21:23:57 +0000

    added raw gva value to properties

--- geo_json_print.py
+++ geo_json_print.py
@@ -1,7 +1,12 @@
 import geojson
 import json
 import csv
-import numpy as np
+import matplotlib as mpl
+import matplotlib.cm as cm
+
+STATISTICS_FILE_LOCATION = "statistics/gva_values.csv"
+NUTS_REGIONS_FILE = "geojson/NUTS_UK.geojson"
+OUTPUT_NUTS_REGIONS_WITH_STATS = 'geojson/NUTS_UK_GVA.geojson'
 
 def read_gva_csv(file_location):
     nut_gva_values = {}
@@ -9,24 +14,21 @@ def read_gva_csv(file_location):
         csv_reader = csv.reader(gva_values_csv)
         for row in csv_reader:
             nut_gva_values[row[0]]=row[1]
-    print(nut_gva_values)
-    normalised_gva_values = normalise_gva_values(nut_gva_values)
-    print(normalised_gva_values)
-    return normalised_gva_values #map_gva_to_colour(gva_values) # THINK HOW TO
+    return normalise_gva_values(nut_gva_values)
 
 
 def normalise_gva_values(nut_gva_values):
     max_value = max([int(i) for i in nut_gva_values.values()])
     min_value = min([int(i) for i in nut_gva_values.values()])
-    print('max_gva_value', max_value)
-    print('min_gva_value', min_value)
+    #print('max_gva_value', max_value)
+    #print('min_gva_value', min_value)
     for nut_gva_value in nut_gva_values:
-        print('nut_code', nut_gva_value)
-        print('gva_value', nut_gva_values[nut_gva_value])
+        #print('nut_code', nut_gva_value)
+        #print('gva_value', nut_gva_values[nut_gva_value])
         nut_gva_values[nut_gva_value]= (int(nut_gva_values[nut_gva_value])  - min_value)/(max_value - min_value)
     return nut_gva_values
 
-def map_value_to_colour(gva_value):
+def generate_colour(gva_value):
     #TODO:  MAPPING FROM NORMALISED NUMBERS TO HEXADECIMAL COLOURS
     return_colour = "green"
     if gva_value > 0.2:
@@ -41,21 +43,26 @@ def map_gva_to_colour(gva_values, geojson):
     #print('nuts_code: ', nuts_code)
     gva_value = gva_values.get(nuts_code, 0) # retrieve gva value, sensible default of 0 if not found
     print('gva_value: ', gva_value)
-    return map_value_to_colour(gva_value)
+    return generate_colour(gva_value)
+
+def gather_raw_statistic(gva_values, geojson):
+    nuts_code = geojson.properties["NUTS312CD"]
+    return gva_values.get(nuts_code, 0) # retrieve gva value, sensible default of 0 if not found
+
 
 # open geojson file
-with open("geojson/NUTS_UK.geojson") as json_file:
+with open(NUTS_REGIONS_FILE) as json_file:
     geo_json_data = geojson.load(json_file)
 
 # READ IN STATISTICS
-gva_values = read_gva_csv("statistics/gva_values.csv")
+gva_values = read_gva_csv(STATISTICS_FILE_LOCATION)
 
 # update colour based off caluclated statistics
 for feature in geo_json_data.features:
-    feature.properties["fill"] = map_gva_to_colour(gva_values, feature)
-    print
-
+    gva_value = gather_raw_statistic(gva_values, feature)
+    feature.properties["fill"] = generate_colour(gva_value)
+    feature.properties['GVA'] = gva_value
 
 # SAVE FILE TO OUTPUT
-with open('geojson/NUTS_UK_GVA.geojson', 'w') as output_json_file:
+with open(OUTPUT_NUTS_REGIONS_WITH_STATS, 'w') as output_json_file:
     json.dump(geo_json_data, output_json_file)
