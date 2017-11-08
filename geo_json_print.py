79d343d07e73b8950aaa99d4c6db25a8532deb66
Joseph Walton <jwgwalton@gmail.com>
2017-11-08 17:30:53 +0000

    initial attempt at colouring the map

--- geo_json_print.py
+++ geo_json_print.py
@@ -1,6 +1,26 @@
 import geojson
+import json
 
+GVA_VALUES = [{ "UKC11": 1000}, {"UKC12": 1001}]
+
+def assign_gva_values(gva_values, geojson):
+    for value in gva_values:
+        print('value', value)
+
+# open geojson file
 with open("data/NUTS_UK.geojson") as json_file:
-    json_data = geojson.load(json_file)
+    geo_json_data = geojson.load(json_file)
 
-print(json_data.keys())
+# update colour based off caluclated statistics
+# TODO: ACTUALLY PULL STATISTICS AND THEN MAP TO A COLOUR
+for feature in geo_json_data.features:
+    #print(feature)
+    feature.properties["fill"] = "red"
+
+# SAVE FILE TO OUTPUT
+with open('data/NUTS_UK_GVA.geojson', 'w') as output_json_file:
+    json.dump(geo_json_data, output_json_file)
+
+
+
+#assign_gva_values(GVA_VALUES, geo_json_data)
