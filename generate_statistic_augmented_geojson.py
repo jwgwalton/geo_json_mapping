72b5420dc3c3ac998e7b2020ad0c4f6f3dee6546
Joseph Walton <jwgwalton@gmail.com>
2017-11-09 09:39:26 +0000

    more nuts geojson

--- generate_statistic_augmented_geojson.py
+++ generate_statistic_augmented_geojson.py
@@ -5,7 +5,7 @@ import matplotlib.cm as cm
 import matplotlib.colors as colors
 import copy as cp
 
-STATISTICS_FILE_LOCATION = "statistics/gva_values.csv"
+STATISTICS_FILE_LOCATION = "statistics/nuts3_gva_values.csv"
 NUTS_REGIONS_FILE = "geojson/NUTS3_UK.geojson"
 OUTPUT_NUTS_REGIONS_WITH_STATS = 'geojson/NUTS3_UK_GVA.geojson'
 STATISTIC_NAME = 'GVA in Millions'
@@ -26,13 +26,6 @@ def normalise_values(values):
         values[value]= (int(values[value])  - min_value)/(max_value - min_value)
     return values
 
-def generate_colour(statistic_value):
-    #TODO:  MAPPING FROM NORMALISED NUMBERS TO HEXADECIMAL COLOURS
-    colour_value = 255*statistic_value*10
-    print('colour_value', colour_value)
-    #return 'rgb(0,'+str(255*(1-statistic_value))+', ' + str(255*statistic_value)+')'
-    return 'rgb(0,0, ' + str(colour_value)+')'
-    #return (0, 255*(1-statistic_value), 255*statistic_value)
 
 def gather_statistic(statistic_values, geojson):
     nuts_code = geojson.properties["NUTS312CD"]
@@ -47,37 +40,20 @@ with open(NUTS_REGIONS_FILE) as json_file:
 unnormalised_values = read_csv(STATISTICS_FILE_LOCATION)
 normalised_values = normalise_values(cp.copy(unnormalised_values))
 
-# create colou map
-max_value = max([int(i) for i in unnormalised_values.values()])
-min_value = min([int(i) for i in unnormalised_values.values()])
+normalised_max =1
+normalised_min = 0
+norm = colors.Normalize(vmin=normalised_min, vmax=normalised_max)
+rgb_colour_mapper = cm.ScalarMappable(norm=norm, cmap=cm.get_cmap('RdYlGn'))
 
-#FUNCTION FOR
-#norm = colors.Normalize(vmin=min_value, vmax=max_value)
-#print("norm: ", norm)
-#to_rgb = cm.ScalarMappable(norm=norm, cmap=cm.get_cmap('RdYlGn'))
-
-
-
-#statistic_values = [int(i) for i in unnormalised_values.values()]
-#colour_values = to_rgb.to_rgba(statistic_values)[:3]
-#print('colour_values: ', colour_values)
-#colour_encoded_regions
-
-norm = colors.Normalize(vmin=0, vmax=1)
-f2rgb = cm.ScalarMappable(norm=norm, cmap=cm.get_cmap('RdYlGn'))
-
-def f2hex(f2rgb, f):
-    rgb = f2rgb.to_rgba(f)[:3]
-    print('rgb: ', rgb)
-    return '#%02x%02x%02x' % tuple([int(255*fc) for fc in rgb]) #tuple([255*fc for fc in rgb])
+def generate_hex_colour(colour_mapper, value):
+    rgb = colour_mapper.to_rgba(value)[:3]
+    return '#%02x%02x%02x' % tuple([int(255*colour) for colour in rgb])
 
 # update colour based off statistic and append statistic as property
 for feature in geo_json_data.features:
     normalised_value = gather_statistic(normalised_values, feature)
     unnormalised_value = gather_statistic(unnormalised_values, feature)
-    colour_value = f2hex(f2rgb, normalised_value)
-    print('colour_value: ', colour_value)
-    feature.properties["fill"] = colour_value#"rgb"+str(colour_value)+")"
+    feature.properties["fill"] = generate_hex_colour(rgb_colour_mapper, normalised_value)
     feature.properties[STATISTIC_NAME] = unnormalised_value
 
 # SAVE FILE TO OUTPUT FILE
